9fdbe26d464fd11d19c34618e1458a8704d231e6
Ratsi0ry <tsiorymonja411@gmail.com>
2026-07-26 08:53:35 +0300

    ui apprentissage

--- fy-force-frontend/src/App.jsx
+++ fy-force-frontend/src/App.jsx
@@ -1,45 +1,76 @@
 import { useState, useEffect } from "react";
 import Login from "./components/Login";
+import Learning from "./components/Learning";
+
+//Mock Data
+const initialLessons = [
+  {
+    id: 1,
+    title: "Apprendre Javascript",
+    modules: [
+      { id: 101, title: "Module 1: Tache 1", completed: false },
+      { id: 102, title: "Module 2: Tache 2", completed: false } //vue actuelle
+    ]
+  },{
+    id: 2,
+    title: "Faire le projet C#",
+    modules: [
+      { id: 101, title: "Module 1: Tache 1", completed: false },
+      { id: 102, title: "Module 2: Tache 2", completed: false } //vue actuelle
+    ]
+  },{
+    id: 3,
+    title: "Apprendre Java",
+    modules: [
+      { id: 101, title: "Module 1: Tache 1", completed: false },
+      { id: 102, title: "Module 2: Tache 2", completed: false } //vue actuelle
+    ]
+  }
+];
 
 export default function App() {
   const [user, setUser] = useState(null);
-  const [showWelcome, setShowWelcome] = useState(false);
+  const [currentView, setCurrentView] = useState('login');
 
   const handleLoginSuccess = (userData) => {
     setUser(userData);
-    setShowWelcome(true);
+    setCurrentView('welcome');
   };
 
   // timer 1s
   useEffect(() => {
-    if (showWelcome) {
+    if (currentView === 'welcome') {
       const timer = setTimeout(() => {
-        setShowWelcome(false);
-        setUser(null); 
+        setCurrentView('learning');
       }, 1000);
-
-      return () => clearTimeout(timer);
+      return ()=> clearTimeout(timer);
     }
-  }, [showWelcome]);
+  }, [currentView]);
 
   return (
     <main style={styles.container}>
-      {showWelcome ? (
+      {currentView === 'login' && (
+        <Login onLoginSuccess={handleLoginSuccess}/>
+      )}
+
+      {currentView === 'welcome' && (
         <div className="glass-card" style={styles.welcomeCard}>
           <div className="auth-header">
             <h1 className="auth-title">Bienvenue, {user?.name}</h1>
             <p className="auth-subtitle">Identifiant : {user?.email}</p>
           </div>
         </div>
-      ) : (
-        <Login onLoginSuccess={handleLoginSuccess} />
+      )}
+
+      {currentView === 'learning' && (
+        <Learning lessonsData={initialLessons} />
       )}
     </main>
   );
 }
 
 const styles = {
-  container: {
+  loginContainer: {
     minHeight: '100vh',
     width: '100%',
     backgroundColor: 'var(--bg-primary)', 
@@ -48,7 +79,14 @@ const styles = {
     justifyContent: 'center',
     padding: '1rem',
     color: '#f9fafb',
-    fontFamily: 'system-ui, sans-serif'
+  },
+  
+  learningContainer: {
+    minHeight: '100vh',
+    width: '100%',
+    backgroundColor: 'var(--bg-primary)', 
+    padding: '2rem',
+    color: '#f9fafb',
   },
   welcomeCard: {
     textAlign: 'center'
